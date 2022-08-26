b0cb6088d16390ff8210520beadde090a40e5fc3
西都凉 <shengyss@163.com>
2022-08-26 21:26:53 +0800

    完成clash自动配置功能

--- api/clash.mjs
+++ api/clash.mjs
@@ -1,42 +1,34 @@
+//https://clash.gitbook.io/doc/restful-api
 import got from 'got';
 class Options {
-    constructor(method, path, opt) {
+    constructor(method, path, opt, data = null) {
         let { port, secret } = opt
         this.url = `http://localhost:${port}${path}`
         this.method = method
         this.headers = {
             Authorization: `Bearer ${secret}`
         }
+        if (data) {
+            this.body = `${data}`
+            this.headers['Content-Type'] = 'application/json'
+        }
+
     }
-    retry = {limit:2}
-    timeout = {request: 3000}
+    retry = { limit: 2 }
+    // timeout = {request: 3000}
 }
 export const getAllProxies = (opt) => {
     let opts = new Options('GET', '/proxies', opt)
     let { url } = opts
     return got(url, { ...opts })
 }
-export const testProxies = (opt, name) => {
-    let opts = new Options('GET', `/proxies/${name}/delay`, opt)
+export const testProxies = (opt, name, suffix = 'com') => {
+    let opts = new Options('GET', `/proxies/${encodeURIComponent(name)}/delay?timeout=5000&url=https://translate.google.${suffix}`, opt)
     let { url } = opts
     return got(url, { ...opts })
 }
-export const selectProxies = (opt) => {
-    let opts = new Options('PUT', `/proxies/${name}/delay`, opt)
+export const selectProxies = (opt, selector, name) => {
+    let opts = new Options('PUT', `/proxies/${encodeURIComponent(selector)}`, opt, JSON.stringify({ name }))
     let { url } = opts
-    return got(url, { ...opts })
+    return got(url, { ...opts }).catch(res => console.log(res))
 }
-
-
-    // getAllProxies({
-    //     "port": 57174,
-    //     "secret": "1f91f3d0-ce09-4c4c-b6b2-9544e625745a"
-    // }).then((res) => {
-    //     // console.log(res);
-        
-    // }).catch((err) => {
-    //     console.log(err);
-        
-    // }); 
-
-
--- index.mjs
+++ index.mjs
@@ -14,16 +14,22 @@ let list = {}//翻译源文件
 let trans = {}//已翻译内容
 let proxy = 'http://localhost:代理端口'//代理地址，可避免开全局代理
 let suffix = 'com'//谷歌翻译网址,默认为com
+let origin = 'auto'//源语言
+let target = 'zh-cn'//目标语言
 let loop//请求定时器
 let globalProxyAgent//全局代理
 let maxLength = 4000//单次翻译文本上限，不建议超过5000，过高会导致漏翻
 let tryNum//重试合并数量
 let clash = {
     port: null,//clash控制端口，见于配置文件或Clash Core后括号内数字
-    secret: null//clash的secret,见于配置文件或web ui设置
+    secret: null,//clash的secret,见于配置文件或web ui设置
+    selector: [""]//策略组
 }
-let proxies//代理节点列表
-
+let proxies = null//代理节点列表
+let proxiesFilter = ["Direct", "Reject", "Selector", "Fallback", "URLTest"]//过滤节点
+let usableProxies = []//可用节点
+let clashSelector = [""]//策略组
+let usedProxies = []//已使用节点
 //初始化
 async function init() {
     let res
@@ -47,7 +53,7 @@ async function init() {
     }
     try {
         res = await readFile('./settings.json', 'utf-8')
-        settings = JSON.parse(fs.readFileSync('./settings.json'))
+        settings = JSON.parse(res)
         console.log('按自定义配置运行');
         if (!settings.alias['原词']) {
             alias = Object.keys(settings.alias)
@@ -64,28 +70,43 @@ async function init() {
             tryNum = nums
         }
         if (settings.proxy != 'http://localhost:代理端口') {
-            console.log('已启用固定代理');
+            console.log('已启用代理');
             globalProxyAgent = createGlobalProxyAgent()
             globalProxyAgent.HTTP_PROXY = settings.proxy
             globalProxyAgent.HTTPS_PROXY = settings.proxy
         } else {
-            console.log('未设定代理，请确保网络通畅\n长时间无响应请重启');
+            console.log('未设定代理，请确保网络通畅,长时间无响应请检查网络状态并重启');
         }
         if (settings.suffix) {
             translate.suffix = settings.suffix
         }
-        if (settings.clash.port&&settings.clash.secret) {
+        if (settings.origin && translate.languages[settings.origin]) {
+            console.log(`已设定源语言为${translate.languages[settings.origin]}`);
+            origin = settings.origin
+        } else {
+            console.log('源语言设定错误，按默认自动识别');
+        }
+        if (settings.target && translate.languages[settings.target]) {
+            console.log(`已设定目标语言为${translate.languages[settings.target]}`);
+            target = settings.target
+        } else {
+            console.log('目标语言设定错误，按默认为简中');
+        }
+        if (settings.clash.port && settings.clash.secret) {
             clash.port = settings.clash.port
             clash.secret = settings.clash.secret
             try {
                 res = await getAllProxies(clash)
-                proxies = res.body
-                console.log('clash连接成功');
+                // await writeFile('./proxies.json', res.body)
+                proxies = JSON.parse(res.body, '', '\t').proxies
+                await testClash('init')
             } catch (error) {
-                console.log('clash配置错误，按默认配置运行,请确保网络通畅\n长时间无响应请重启');
+                console.log(error);
+                console.log('clash配置错误,请确保网络通畅,长时间无响应请检查网络状态并重启');
             }
+        } else {
+            console.log('未连接clash，使用静态代理');
         }
-        console.log(`最大合并${nums}条,翻译间隔${loopTime}秒`);
     } catch (error) {
         // console.log(error);
         if (error.message.includes('Unexpected token')) {
@@ -98,23 +119,71 @@ async function init() {
                 maxLength,
                 proxy,
                 suffix,
+                origin,
+                target,
                 alias,
-                clash
+                clash,
             }
             try {
                 res = await writeFile('./settings.json', JSON.stringify(settings, '', '\t'))
-                console.log('已生成默认配置文件settings.json,本次按默认配置运行,请确保网络通畅\n长时间无响应请重启');
+                console.log('已生成默认配置文件settings.json,本次按默认配置运行,请确保网络通畅,长时间无响应请检查网络状态并重启');
                 alias = []
-                console.log(`最大合并${nums}条,翻译间隔${loopTime}秒`);
             } catch (error) {
-                console.log('写入配置失败,按默认配置运行,请确保网络通畅\n长时间无响应请重启');
+                console.log('写入配置失败,按默认配置运行,请确保网络通畅,长时间无响应请检查网络状态并重启');
             }
         }
     }
+    console.log(`最大合并${nums}条,翻译间隔${loopTime}秒`);
     fun(trans)
 }
 init()
 
+//clash自动调整
+async function testClash(met) {
+    let testQueue = []
+    usableProxies = []
+    clashSelector = []
+    for (const name in proxies) {
+        const proxy = proxies[name]
+        if ((!proxiesFilter.includes(proxy.type)) && (!usedProxies.includes(name))) {
+            testQueue.push(testProxies(clash, name).then(res => usableProxies.push(name)).catch(err => Promise.resolve()))
+        }
+        if (proxy.type == 'Selector') {
+            settings.clash.selector.forEach(val => {
+                if (proxy.name.toLowerCase().includes(val.toLowerCase())) {
+                    clashSelector.push(proxy.name)
+                }
+            })
+        }
+    }
+    if (met == 'retry') {
+        console.log('尝试更换代理');
+    }else{
+        console.log('clash连接中');
+    }
+    return Promise.all(testQueue)
+        .then((res) => getAllProxies(clash))
+        .then((res) => {
+            proxies = JSON.parse(res.body, '', '\t').proxies
+            if (met == 'init') {
+                console.log('clash连接成功');
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            console.log('clash连接失败');
+        })
+        .then(res => {
+            return selectProxies(clash, clashSelector[0], usableProxies[0])
+        })
+        .then(res => {
+            usedProxies.push(usableProxies[0])
+            console.log(`已选择策略组：${clashSelector[0]}，已设置代理为：${usableProxies[0]}`);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
 
 //测试用
 function test() {
@@ -226,7 +295,7 @@ function fun(trans) {
         }
     }
     // console.log(txt.length, start);
-    translate(txt, { to: 'zh-cn' }).then((res) => {
+    translate(txt, { from: origin, to: target }).then((res) => {
         res = res.text.split('\n')
         // console.log(res.length);
         if (upload != res.length) {
@@ -250,10 +319,15 @@ function fun(trans) {
             })
         }
     }).catch((err) => {
-        console.log(err);
-        console.log('请检查网络，尝试更换ip');
-        clearTimeout(loop)
         fs.writeFile('./TrsData.json', JSON.stringify(trans, '', '\t'), err => { })
-    });
+        return testClash('retry')
+            .then(res => {
+                clearTimeout(loop)
+                fun(trans)
+            }).catch(err => {
+                console.log('请检查网络，尝试更换ip或代理');
+                clearTimeout(loop)
+            })
+    })
 }
 
